c80984b8f0aa4e9286174a30f6a0187e6da4c9d3
David Medel <david.medel@sansano.usm.cl>
2020-05-03 17:07:32 -0400

    codigo limpiado

--- cliente.py
+++ cliente.py
@@ -1,4 +1,6 @@
 import socket as sock
+
+#Función para crear un archivo por header consultado.
 def escribir_archivo(nombre_archivo,header):
 	archivo = open(nombre_archivo,"w")
 	archivo.write(header)
@@ -13,35 +15,33 @@ while(flag):
 	#creamos el socket del cliente
 	TCP_socketCliente = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
 
-	#esta fuención realiza handshake
+	#Se realiza el handshake para conexión con el servidor
 	TCP_socketCliente.connect((dir_servidor, puerto_servidor))
 
+	#Solicitamos la url al usuario
 	url = input('Ingrese la URL: ')
 
 	if(url.upper() == "TERMINATE"):
-		print("terminar ")
 		flag= False
-		TCP_socketCliente.send(url.encode())
-		TCP_socketCliente.close()
-	else:
+		TCP_socketCliente.send(url.encode()) #notificamos al servidor para que cierre la conexión
+		TCP_socketCliente.close() #se cierra el socket cliente
 
+	else:
+		#enviamos el url al servidor para la consulta
 		TCP_socketCliente.send(url.encode())
 		puerto_z = TCP_socketCliente.recv(2048).decode()
-		print("la conexión UDP sera en el puerto",puerto_z)
-
+		#print("la conexión UDP sera en el puerto",puerto_z)
 
 		#conexión UDP con el servidor
 		UDP_socketCliente = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
-		respuesta_conexion = "OK"
+		respuesta_conexion = "OK" #respondemos OK al servidor para la conexión UDP.
 		UDP_socketCliente.sendto(respuesta_conexion.encode(),(dir_servidor,int(puerto_z)))
 
+		#obtenemos el header mediante la respuesta por UDP del servidor.
 		header, _ = UDP_socketCliente.recvfrom(2048)
 		str_header = header.decode()
-		#escribir el header en el .txt
-		nombre_archivo = url + ".txt"
-		escribir_archivo(nombre_archivo,str_header)
-
-		#print(str_header)
-		UDP_socketCliente.close()
-
 		
+		nombre_archivo = url + ".txt"
+		escribir_archivo(nombre_archivo,str_header) #creamos el archivo con el header
+
+		UDP_socketCliente.close() #cerramos socket cliente	
--- server.py
+++ server.py
@@ -1,17 +1,18 @@
 import socket as sock
 
-#Rango de puertos 49152-65535
-
+#Rango de puertos disponibles 9152-65535 
 puerto_servidor =65000
 
+#Función que agrega la tupla (url, header) al cache del servidor
 def add_cache(lista, elemento):
     lista.insert(0,elemento)
     if len(lista) > 5:
         lista.pop(5)
     return
 
+#Función que verifica si el url ya fue consultado, 
+#retorna una tupla con el valor de la condición y el header
 def in_cache(url, cache):
-#verifica si el elemento ya se encuentra en el cach
 	for i in cache:
 		if i[0] == url:
 			header = i[1]
@@ -20,60 +21,57 @@ def in_cache(url, cache):
 			return (True, header)
 	return (False,'')
 
-
-
-#creamos socket TCP bruch
+#creamos socket TCP
 TCP_socketServ = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
 TCP_socketServ.bind(('', puerto_servidor))
 #indica que espera handshakes, parametro que indica cantidad maxima de cola
 
-#Servidor comienza a escuchar siempre queda activo.
-TCP_socketServ.listen(5) #para el manejo de cache LRU
+TCP_socketServ.listen(5) #Servidor comienza a escuchar siempre queda activo
 print("Servidor TCP escuchando en puerto", puerto_servidor)
 
 flag = True
 cache  = []
 termino = 0
 while(flag):
-	#solo recibe archivos comunicación
+	#se obtiene datos del cliente
 	TCP_socketCliente, dirCliente = TCP_socketServ.accept()
 	#se recibe la URL 
 	url = TCP_socketCliente.recv(2048).decode()
-	print("Se recibio:", url)
+	#print("Se recibio:", url)
 
-	condicion, header_cache = in_cache(url,cache)
+	condicion, header_cache = in_cache(url,cache) #url esta en el cache?
 
 	if (url.upper() == "TERMINATE"):
 		TCP_socketCliente.close()
 		termino =1
-		#flag = False
-		#se finaliza la conexión del servidor
-		#obtener su header y moficiar posicion de cache
+		#flag = False  se cerraría la conexión del servidor
 	elif (condicion):
 		header = header_cache
 	else:
-		#Se obtiene el header con conexión directa a la web
+		#Se obtiene el header con conexión directa a la web mediante TCP
 		httpServ = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
 		httpServ.connect((url, 80))
-		consulta = "GET / HTTP/1.1\r\n\r\n"
+		consulta = "GET / HTTP/1.1\r\n\r\n" #Consulta http
 		httpServ.send(consulta.encode())
 
 		respuesta_consulta = httpServ.recv(2048)
-		rc_decode = respuesta_consulta.decode().split("<")
+		rc_decode = respuesta_consulta.decode().split("<") #obtenemos solo el header
 		header= rc_decode[0].encode()
-		add_cache(cache,(url,header))
+		add_cache(cache,(url,header)) #agregamos la respuesta al cache del servidor
 		httpServ.close()
 
 	if termino == 0: 
-		print(cache)	
+
+		#print(cache) #para visualizar su funcionamiento
 		#se envia nuevo puerto donde existirá la conexión UDP
 		puerto_z = '55000'
 		#se responde por TCP
 		TCP_socketCliente.send(puerto_z.encode())
 		UDP_socketServ = sock.socket(sock.AF_INET, sock.SOCK_DGRAM)
 		UDP_socketServ.bind(('', int(puerto_z)))
-		print("Servidor escuchando en el puerto", puerto_z)
-		#ahora aqui escucha en el servidor UDP y recibe.
+		#print("Servidor escuchando en el puerto", puerto_z)
+
+		#ahora aqui escucha en el servidor UDP y recibe la respuesta OK del cliente.
 		respuestaUDP, dirClienteUDP = UDP_socketServ.recvfrom(2048)
 		respUDP = respuestaUDP.decode()
 		if (respUDP == "OK"):
@@ -82,11 +80,5 @@ while(flag):
 			UDP_socketServ.close()
 	else:
 		termino = 0
-		#recibe OK y le envia el header
-		#se genera una nueva conexión UDP
-		#enviar header de URL mediante UDP
-
-		#aqui crear ciclo hasta que diga terminate.
-
-		#ver el cierre 
-		#TCP_socketCliente.close()
+		#cuando el cliente ingresa "terminate" se finaliza su conexión pero el servidor sigue
+		#activo esperando otro cliente.
